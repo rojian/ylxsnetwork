2aa01f09ea555d34c8459b15385400a28022f442
rojian <1184571251@qq.com>
2016-09-26 15:21:47 +0800

    dao方法接口

--- src/com/tosit/ylxs/dao/ImplementDao.java
+++ src/com/tosit/ylxs/dao/ImplementDao.java
@@ -1,8 +1,34 @@
 package com.tosit.ylxs.dao;
 
+import com.tosit.ylxs.entity.User;
+
 /**
  * Created by Administrator on 2016/9/26.
  */
 public interface ImplementDao {
-    public boolean adduser();
+    /**
+     * 添加用户
+     * @return 判断是否创建成功
+     */
+    public boolean addUser(User user);
+
+    /**
+     * 删除用户
+     * @return 判断是否删除成功
+     */
+    public boolean delUser(User user);
+
+    /**
+     * 更改用户
+     * @return 判断是否更改成功
+     */
+    public boolean upUser(User user);
+
+    /**
+     * 查询用户
+     * @param user 是否存在该用户
+     * @return
+     */
+    public boolean findUser(User user);
+
 }
---
+++ src/com/tosit/ylxs/entity/User.java
@@ -0,0 +1,7 @@
+package com.tosit.ylxs.entity;
+
+/**
+ * Created by Administrator on 2016/9/26.
+ */
+public class User {
+}
