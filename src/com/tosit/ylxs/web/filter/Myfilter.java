529b570498c0b93de074cf46f54cca83411db771
rojian <1184571251@qq.com>
2016-09-28 09:13:48 +0800

    数据库操作

---
+++ src/com/tosit/ylxs/web/filter/Myfilter.java
@@ -0,0 +1,18 @@
+package com.tosit.ylxs.web.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2016/9/27.
+ */
+public class Myfilter implements javax.servlet.Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("utf-8");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+}
