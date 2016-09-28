17fa23467666009f256e08d725c3c636f2e29901
rojian <1184571251@qq.com>
2016-09-28 11:35:54 +0800

    shanchu

--- src/com/tosit/ylxs/web/controller/IndexServlet.java
+++ src/com/tosit/ylxs/web/controller/IndexServlet.java
@@ -1,1 +1,38 @@
 
+package com.tosit.ylxs.web.controller;
+
+import com.google.gson.Gson;
+import com.tosit.ylxs.dao.ArticleDao;
+import com.tosit.ylxs.entity.Article;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 通过板块查询Article数据
+ * Created by Administrator on 2016/9/26.
+ */
+@WebServlet("/indexServlet/*")
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//       resp.setHeader("Content-type","text/html;charset=utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/json;charset=utf-8");
+        PrintWriter out =resp.getWriter();
+        String plate=req.getParameter("plate");
+//        System.out.println(plate);
+        Gson gson=new Gson();
+        ArticleDao articleDao=new ArticleDao();
+        List<Article> articles=articleDao.selectArticleByPlate(plate);
+        String jsonObjects=gson.toJson(articles);
+        out.write(jsonObjects);
+    }
+}
