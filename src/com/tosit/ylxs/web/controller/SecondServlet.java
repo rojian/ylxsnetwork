51619f8e1a2427ff48f88b230a7b4b5a45db7444
rojian <1184571251@qq.com>
2016-09-29 16:49:22 +0800

    shanchu

--- src/com/tosit/ylxs/web/controller/SecondServlet.java
+++ src/com/tosit/ylxs/web/controller/SecondServlet.java
@@ -12,14 +12,15 @@ import java.io.PrintWriter;
  * 跳转到2级页面
  * Created by Administrator on 2016/9/29.
  */
-@WebServlet("/*/activity")
+@WebServlet("*.do")
 public class SecondServlet extends HttpServlet{
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out=resp.getWriter();
 //        通过id查询Article数据
         int id = Integer.parseInt(req.getParameter("id"));
         String number=req.getParameter("number");
+        System.out.println(number);
 //        把id如session
         req.getSession().setAttribute("id",id);
         resp.sendRedirect("http://localhost:8080/"+number+"/activity.html");
